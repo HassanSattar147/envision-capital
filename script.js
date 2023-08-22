91e1006a93bef97b30cc223e9bddadcf8b30ed28
HassanSattar147 <53467534+HassanSattar147@users.noreply.github.com>
2023-08-22 16:00:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,33 +2,33 @@
 let nav_container = document.querySelector('.header__nav');
 let humberger_btn = document.querySelector('#humberger-btn');
 
-humberger_btn.addEventListener('click', () =>{
+humberger_btn.addEventListener('click', () => {
     nav_container.classList.toggle('active');
 
-    if(humberger_btn.classList.contains('fa-bars')){
-        
+    if (humberger_btn.classList.contains('fa-bars')) {
+
         humberger_btn.classList.remove('fa-bars');
         humberger_btn.classList.add('fa-xmark');
 
-    }else{
+    } else {
         humberger_btn.classList.add('fa-bars');
         humberger_btn.classList.remove('fa-xmark');
 
     }
 })
 
-window.addEventListener('click', function(e){
-    if(e.target != nav_container && e.target != humberger_btn){
+window.addEventListener('click', function (e) {
+    if (e.target != nav_container && e.target != humberger_btn) {
 
-        if(nav_container.classList.contains('active')){
+        if (nav_container.classList.contains('active')) {
             nav_container.classList.remove('active');
-        }  
+        }
 
-        if(humberger_btn.classList.contains('fa-xmark')){
+        if (humberger_btn.classList.contains('fa-xmark')) {
 
             humberger_btn.classList.add('fa-bars');
             humberger_btn.classList.remove('fa-xmark');
-    
+
         }
     }
 })
